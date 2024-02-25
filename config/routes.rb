6f82d09b1889de8b9df652ef2fe491ae5eef9e39
Abhishek87654321 <abhishekgangdhari@gmail.com>
2024-02-25 17:13:09 +0530

    generate schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'pages/home' ,to: 'pages#home'
+  get 'pages/home', to: 'pages#home'
   root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
